feb375902d9cdafe3291d62d75b886f16b3019dd
Jonathan Beckett <jonathan.beckett@gmail.com>
2023-11-12 17:28:26 +0000

    Update stations.py

--- stations.py
+++ stations.py
@@ -1,8 +1,6 @@
 import json
 
 print("Reading Stations File")
-with open('input/stations.json', encoding='utf-8') as stations_file:
-  stations = json.load(stations_file)
 
 def clean_url(url):
   return url.replace(";","")
@@ -15,27 +13,24 @@ def encodeXMLText(text):
     text = text.replace(">", "&gt;")
     return text
 
-def get_countrycodes():
-  codes = [s['countrycode'].upper() for s in stations]
-  return sorted(list(set(codes)))
-
-def create_countries_csv_file():
-  print("Creating Countries CSV file")
+def get_countries(stations):
   countries = ([(s['countrycode'].upper(), s['country']) for s in stations])
   countries = sorted(list(set(countries)))
+  return countries
+
+def create_countries_csv_file(countries):
+  print("Creating Countries CSV file")
   with open('output/countries.csv', 'w', encoding='utf-8') as countries_file:
     for country in countries:
       countries_file.write("\"" + country[0] + "\",\"" + country[1] + "\"\n")
 
-def create_countries_txt_file():
+def create_countries_txt_file(countries):
   print("Creating Countries TXT file")
-  countries = ([(s['countrycode'].upper(), s['country']) for s in stations])
-  countries = sorted(list(set(countries)))
   with open('output/countries.txt', 'w', encoding='utf-8') as countries_file:
     for country in countries:
       countries_file.write(country[0] + " " + country[1] + "\n")
 
-def create_m3u_file(countrycode):
+def create_m3u_file(countrycode, stations):
   print("Creating M3U file for " + countrycode)
   with open('output/m3u/radio_stations_' + countrycode + '.m3u', 'w', encoding='utf-8') as output_file:
     output_file.write("#EXTM3U\n")
@@ -47,8 +42,11 @@ def create_m3u_file(countrycode):
           output_file.write(clean_url(s['url']) + '\n')
         last_name = s['name']
 
-def create_pls_file(countrycode):
+
+def create_pls_file(countrycode, stations):
+
   print("Creating PLS file for " + countrycode)
+
   with open('output/pls/radio_stations_' + countrycode + '.pls', 'w', encoding='utf-8') as output_file:
     output_file.write("[playlist]\n")
     i = 0
@@ -61,20 +59,24 @@ def create_pls_file(countrycode):
       last_name = s['name']
     output_file.write("NumberOfEntries=" + str(i))
 
-def create_opml_file():
+
+def create_opml_file(countries, stations):
+  
   print("Creating OPML file")
+
   with open('output/internet_radio_stations.opml', 'w', encoding='utf-8') as opml_file:
+
     opml_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
     opml_file.write("<opml version=\"1.0\">\n")
     opml_file.write("<head>\n<title>Internet Radio Stations</title>\n</head>\n<body>\n")
     
-    countries = ([(s['countrycode'].upper(), s['country']) for s in stations])
-    countries = sorted(list(set(countries)))
-
     for country in countries:
+
       opml_file.write("<outline title=\"" + encodeXMLText(country[1]) + "\" text=\"" + encodeXMLText(country[1]) + "\">\n")
       last_name = ""
+      
       for s in stations:
+
         if (s['name'] != last_name) and (s['lastcheckok'] == 1) and (s['countrycode'].upper() == country[0]) and (s['codec'] == 'MP3'):
           opml_file.write("<outline text=\"" + encodeXMLText(s['name']) + "\" title=\"" + encodeXMLText(s['name']) + "\" type=\"audio\" url=\"" + encodeXMLText(clean_url(s['url'])) + "\" />\n")
         last_name = s['name']
@@ -83,16 +85,16 @@ def create_opml_file():
 
     opml_file.write("</body>\n</opml>\n")
 
-def create_html_file():
+
+def create_html_file(countries, stations):
+
   print("Creating HTML file")
+  
   with open('output/internet_radio_stations.htm', 'w', encoding='utf-8') as html_file:
     
     html_file.write("<html>\n<head>\n<title>Internet Radio Stations</title>\n</head>\n")
     html_file.write("<body>\n")
     
-    countries = ([(s['countrycode'].upper(), s['country']) for s in stations])
-    countries = sorted(list(set(countries)))
-
     html_file.write("<h1>Internet Radio Stations</h1>")
 
     html_file.write("<h2>Countries</h2>")
@@ -130,13 +132,20 @@ def create_html_file():
 
     html_file.write("</body>\n</html>\n")
 
-create_countries_csv_file()
-create_countries_txt_file()
-create_html_file()
-create_opml_file()
 
-codes = get_countrycodes()
-for code in codes:
-  create_m3u_file(code.upper())
-  create_pls_file(code.upper())
+
+with open('input/stations.json', encoding='utf-8') as stations_file:
+  stations = json.load(stations_file)
+
+  countries = get_countries(stations)
+
+  create_countries_csv_file(countries)
+  create_countries_txt_file(countries)
+
+  create_html_file(countries, stations)
+  create_opml_file(countries, stations)
+
+  for country in countries:
+    create_m3u_file(country[0], stations)
+    create_pls_file(country[0], stations)
 
